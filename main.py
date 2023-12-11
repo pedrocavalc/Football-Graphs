0b10801b0ec454e7dad10532ea01e73f64dfb6ae
Carlos Henrique <carlos.h.computacao@gmail.com>
2023-12-11 15:06:07 -0300

    posições randomizadas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint, shuffle, uniform
+import threading
 import pygame
 from typing import List, Tuple, Any
 
@@ -15,14 +17,23 @@ def process_event_bus():
 
 
 def get_player_positions() -> List[Tuple[int, int]]:
-    """Função temporária, que retorna um exemplo
-    de posicionamento para os jogadores."""
-    return [(0, 0),
-            (1, -2), (1, -1), (1, 1), (1, 2),
-            (2, -1), (2, 1),
+    """Função temporária que retorna um exemplo de posicionamento para os jogadores."""
+    positions = [(0, 0),
+                 (1, -2), (1, -1), (1, 1), (1, 2),
+                 (2, -1), (2, 1),
+                 (4, -2), (4, 2),
+                 (5, -1), (5, 1)]
 
-            (4, -2), (4, 2),
-            (5, -1), (5, 1)]
+    # Modifica os valores internos de forma aleatória até o valor máximo de 4 antes do embaralhamento
+    for i in range(1, len(positions)):
+        x, y = positions[i]
+        # Exemplo: Modifica y para um valor aleatório entre y-2 e o mínimo entre y+2 e 4
+        positions[i] = (x, round(uniform(-2, 2)))
+
+    # Embaralha as posições excluindo a posição inicial (0, 0)
+    #shuffle(positions[1:])
+
+    return positions
 
 
 def generate_graph(jogadores: List[Any], posicoes: List[Tuple[int, int]]):
@@ -35,6 +46,24 @@ def generate_graph(jogadores: List[Any], posicoes: List[Tuple[int, int]]):
 
     return grafo
 
+def update_positions():
+    global posicoes, grafo
+
+    posicoes = get_player_positions()
+    grafo = generate_graph(jogadores, posicoes)
+    pygame.time.set_timer(pygame.USEREVENT, 5000)  # Define um evento do Pygame para ocorrer a cada 5 segundos
+    grafo = generate_graph(jogadores, posicoes)
+    grafo.adiciona_aresta("Martinelli", "Jesus")
+    grafo.adiciona_aresta("André", "Raphinha")
+    grafo.adiciona_aresta("André", "Guimarães")
+    grafo.adiciona_aresta("André", "Raphinha")
+    grafo.adiciona_aresta("Rodrygo", "Raphinha")
+    grafo.adiciona_aresta("Rodrygo", "Martinelli")
+    grafo.adiciona_aresta("Royal", "André")
+    grafo.adiciona_aresta("Marquinhos", "André")
+    grafo.adiciona_aresta("Royal", "Rodrygo")
+    grafo.visualizar()
+
 
 if __name__ == "__main__":
     TAMANHO_TELA = (1600, 900)
@@ -44,27 +73,44 @@ if __name__ == "__main__":
                  "Augusto", "André", "Guimarães", "Rodrygo",
                  "Raphinha", "Jesus", "Martinelli"]
     posicoes = get_player_positions()
+    threading.Timer(5.0, __name__).start()
+    pygame.init()
+    update_positions()
 
     grafo = generate_graph(jogadores, posicoes)
-    grafo.adiciona_aresta("Martinelli", "Jesus", 1)
-    grafo.adiciona_aresta("André", "Raphinha", 1)
-    grafo.adiciona_aresta("André", "Guimarães", 1)
-    grafo.adiciona_aresta("André", "Raphinha", 1)
-    grafo.adiciona_aresta("Rodrygo", "Raphinha", 1)
-    grafo.adiciona_aresta("Rodrygo", "Martinelli", 1)
-    grafo.adiciona_aresta("Royal", "André", 1)
-    grafo.adiciona_aresta("Marquinhos", "André", 1)
-    grafo.adiciona_aresta("Royal", "Rodrygo", 1)
+    grafo.adiciona_aresta("Martinelli", "Jesus")
+    grafo.adiciona_aresta("André", "Raphinha")
+    grafo.adiciona_aresta("André", "Guimarães")
+    grafo.adiciona_aresta("André", "Raphinha")
+    grafo.adiciona_aresta("Rodrygo", "Raphinha")
+    grafo.adiciona_aresta("Rodrygo", "Martinelli")
+    grafo.adiciona_aresta("Royal", "André")
+    grafo.adiciona_aresta("Marquinhos", "André")
+    grafo.adiciona_aresta("Royal", "Rodrygo")
     grafo.visualizar()
 
-    pygame.init()
+    #pygame.init()
     tela = pygame.display.set_mode(TAMANHO_TELA)
     interface = InterfaceDrawer(tela, "assets/pitch.jpg")
 
     interface.draw_background()
     interface.draw_edges(grafo, (255, 0, 0))
     interface.draw_players(grafo, COR_CIRCULO)
+    #
 
     quit = False
     while not quit:
         quit = process_event_bus()
+        
+        # Atualiza a tela
+        interface.draw_background()
+        interface.draw_edges(grafo, (255, 0, 0))  # Desenha as linhas vermelhas
+        interface.draw_players(grafo, COR_CIRCULO)
+
+        pygame.display.flip()  # Atualiza a tela do Pygame
+        pygame.time.Clock().tick(1)  # Controla a taxa de quadros
+        
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:  # Verifica se ocorreu o evento definido pelo Pygame (a cada 5 segundos)
+                update_positions() 
+
---
+++ teste.py
@@ -0,0 +1,100 @@
+import pygame
+import pygame.time
+from random import shuffle, uniform
+from typing import List, Tuple, Any
+
+from gui.gui import InterfaceDrawer
+from data_structure.graph import GrafoSimples
+
+
+def process_event_bus():
+    """Função que processa a fila de eventos do PyGame. Necessária
+    Para fechar o programa normalmente."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+    return False
+
+
+def get_player_positions() -> List[Tuple[int, int]]:
+    """Função temporária que retorna um exemplo de posicionamento para os jogadores."""
+    positions = [(0, 0),
+                 (1, -2), (1, -1), (1, 1), (1, 2),
+                 (2, -1), (2, 1),
+                 (4, -2), (4, 2),
+                 (5, -1), (5, 1)]
+
+    # Modifica os valores internos de forma aleatória até o valor máximo de 4 antes do embaralhamento
+    for i in range(1, len(positions)):
+        x, y = positions[i]
+        # Exemplo: Modifica y para um valor aleatório entre y-2 e o mínimo entre y+2 e 4
+        positions[i] = (x, round(uniform(-2, 2)))
+
+    # Embaralha as posições excluindo a posição inicial (0, 0)
+    #shuffle(positions[1:])
+
+    return positions
+
+
+def generate_graph(jogadores: List[Any], posicoes: List[Tuple[int, int]]):
+    """Gera um grafo simples, sem arestas, e com os jogadores
+    como vértices."""
+    grafo = GrafoSimples()
+
+    for jogador, posicao in zip(jogadores, posicoes):
+        grafo.adiciona_vertice(jogador, posicao)
+
+    return grafo
+
+
+def update_positions():
+    global posicoes, grafo
+
+    posicoes = get_player_positions()
+    grafo = generate_graph(jogadores, posicoes)
+    pygame.time.set_timer(pygame.USEREVENT, 5000)  # Define um evento do Pygame para ocorrer a cada 5 segundos
+
+
+if __name__ == "__main__":
+    TAMANHO_TELA = (1600, 900)
+    COR_CIRCULO = (255, 255, 255)
+
+    jogadores = ["Alisson", "Royal", "Marquinhos", "Magalhães",
+                 "Augusto", "André", "Guimarães", "Rodrygo",
+                 "Raphinha", "Jesus", "Martinelli"]
+    posicoes = get_player_positions()
+
+    grafo = generate_graph(jogadores, posicoes)
+    grafo.adiciona_aresta("Martinelli", "Jesus")
+    grafo.adiciona_aresta("André", "Raphinha")
+    grafo.adiciona_aresta("André", "Guimarães")
+    grafo.adiciona_aresta("André", "Raphinha")
+    grafo.adiciona_aresta("Rodrygo", "Raphinha")
+    grafo.adiciona_aresta("Rodrygo", "Martinelli")
+    grafo.adiciona_aresta("Royal", "André")
+    grafo.adiciona_aresta("Marquinhos", "André")
+    grafo.adiciona_aresta("Royal", "Rodrygo")
+    grafo.visualizar()
+
+    pygame.init()
+    tela = pygame.display.set_mode(TAMANHO_TELA)
+    interface = InterfaceDrawer(tela, "assets/pitch.jpg")
+
+    update_positions()  # Chamada inicial
+    
+
+    quit = False
+    while not quit:
+        quit = process_event_bus()
+
+        # Atualiza a tela
+        interface.draw_background()
+        interface.draw_edges(grafo, (255, 0, 0))
+        interface.draw_players(grafo, COR_CIRCULO)
+
+        pygame.display.flip()  # Atualiza a tela do Pygame
+        pygame.time.Clock().tick(1)  # Controla a taxa de quadros
+
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:  # Verifica se ocorreu o evento definido pelo Pygame (a cada 5 segundos)
+                update_positions()  # Atualiza as posições
